c2c53fe8d783e4ea25f77998070a6ea89a3251de
JoaoPaulo-LD <joaopauloalencarrodrigues@gmail.com>
2020-10-06 16:36:48 -0300

    Side menu animation finished

--- src/components/SideMenu/index.tsx
+++ src/components/SideMenu/index.tsx
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react'
 
-import { Container } from './styles';
+import { Container } from './styles'
+
+const scrollThreshold = 300
 
 const SideMenu: React.FC = ({ children }) => {
+  const [scrollY, setScrollY] = useState(0)
+  const [isActive, setIsActive] = useState(true)
+
+  useEffect(() => {
+    function onScroll() {
+      setScrollY(window.scrollY)
+      setIsActive(false)
+    }
+
+    window.addEventListener('scroll', onScroll)
+
+    return () => window.removeEventListener('scroll', onScroll)
+  }, [])
+
+  const classes = [
+    isActive ? 'open' : '',
+    scrollY <= scrollThreshold ? 'scrollOpen' : ''
+  ]
+
+  const className = classes.join(' ').trim()
+
   return (
-    <Container>
+    <Container className={className}>
       {children}
     </Container>
-  );
-};
+  )
+}
 
-export default SideMenu;
+export default SideMenu
